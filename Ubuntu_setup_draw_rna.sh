d7ed968f0daa227091ccbcfc8571ea2fe4b88ba9
Shujun-He <38227229+Shujun-He@users.noreply.github.com>
2021-03-07 22:45:41 -0800

    added some stuff
    added git clone bprna, fixed arnie config file

--- Ubuntu_setup_draw_rna.sh
+++ Ubuntu_setup_draw_rna.sh
@@ -1,13 +1,12 @@
 git clone https://github.com/DasLab/arnie arnie;
+git clone https://github.com/hendrixlab/bpRNA bpRNA;
 cd arnie && git reset --hard bdb7f803fe24273c34b3402c3d18036d9bfc882e;
 cd ..;
-echo "vienna_2: /usr/bin" > arnie.conf;
-echo "TMP: tmp" >> arnie.conf;
+echo "TMP: tmp" > arnie.conf;
+echo "rnasoft: MultiRNAFold" >> arnie.conf;
 git clone https://www.github.com/DasLab/draw_rna draw_rna_pkg;
 cd draw_rna_pkg && git reset --hard 90357a5ca529f834b50b07bc8f66ed198e43374d;
 cd ..;
 cp -R fixed_draw.py draw_rna_pkg/ipynb/draw.py;
 cd draw_rna_pkg && python setup.py install;
-
-
-
+chmod u+x run.sh
